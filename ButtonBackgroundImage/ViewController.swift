702698059ff6dad7c4ef79915b2b9fba7660f138
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-24 11:41:07 +0530

    Initial Commit

--- ButtonBackgroundImage/ViewController.swift
+++ ButtonBackgroundImage/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageview: UIImageView!
+    @IBOutlet var show: UIButton!
+    @IBAction func Buttonbackground(_ sender: Any) {
+      imageview.image = UIImage(named: "dhoni2")
+    }
     override func viewDidLoad() {
-        super.viewDidLoad()
+        let image = UIImage(named: "dhoni")?.withRenderingMode(.alwaysOriginal)
+        show.setImage(image, for: UIControlState.normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
